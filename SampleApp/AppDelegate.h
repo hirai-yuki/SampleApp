fe30761b7aecb3f5e422327fae81d9ac3387fb7d
hirai.yuki <hirai.yuki@classmethod.jp>
2015-03-10 02:40:52 +0900

    Initial

---
+++ SampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by hirai.yuki on 2015/03/10.
+//  Copyright (c) 2015年 Classmethod, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by hirai.yuki on 2015/03/10.
+//  Copyright (c) 2015年 Classmethod, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
